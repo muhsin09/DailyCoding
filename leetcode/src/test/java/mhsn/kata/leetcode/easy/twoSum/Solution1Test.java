dd0b85b115c5e04ba9e789e6ff636e2f6314ea9b
muhsin.ozbek <muhsin.ozbek@tubitak.gov.tr>
2020-05-14 15:05:54 +0300

    Day1 - TwoSum problem

---
+++ leetcode/src/test/java/mhsn/kata/leetcode/easy/twoSum/Solution1Test.java
@@ -0,0 +1,27 @@
+package mhsn.kata.leetcode.easy.twoSum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Solution1Test {
+
+  Solution1 solution;
+
+  @BeforeEach
+  void setup(){
+    solution = new Solution1();
+  }
+
+  @Test
+  void twoSum() {
+    int[] nums = new int[]{2, 7, 11, 15};
+    Assertions.assertArrayEquals(new int[]{0, 1}, solution.twoSum(nums, 9));
+  }
+
+  @Test
+  public void twoSumTest_when() {
+    int[] nums = new int[]{3, 2, 4};
+    Assertions.assertArrayEquals(new int[]{1, 2}, solution.twoSum(nums, 6));
+  }
+}
