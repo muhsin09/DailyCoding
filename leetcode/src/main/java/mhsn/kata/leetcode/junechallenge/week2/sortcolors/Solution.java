6f067e46b94c5cf9b012007845f9efb7a70ceea5
muhsinozbek <muhsin.ozbek@tubitak.gov.tr>
2020-06-11 10:51:02 +0300

    Jun challenge Week2 Day4: sort colors

---
+++ leetcode/src/main/java/mhsn/kata/leetcode/junechallenge/week2/sortcolors/Solution.java
@@ -0,0 +1,27 @@
+package mhsn.kata.leetcode.junechallenge.week2.sortcolors;
+
+class Solution {
+
+  public void sortColors(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return;
+    }
+
+    int[] countArray = new int[3];
+    for (int i = 0; i < nums.length; i++) {
+      countArray[nums[i]]++;
+    }
+
+    int j = 0;
+    int k = 0;
+    while (j <= 2) {
+      if (countArray[j] != 0) {
+        nums[k++] = j;
+        countArray[j] = countArray[j] - 1;
+      } else {
+        j++;
+      }
+    }
+
+  }
+}
